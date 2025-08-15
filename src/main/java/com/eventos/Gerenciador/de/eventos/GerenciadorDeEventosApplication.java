edc9eaedbe74eb18b16c951196c8a8e695a6023c
eduardosackser07 <martinseduardo261@gmail.com>
2025-08-15 18:59:14 -0300

    os arquivo inicial ai po

---
+++ src/main/java/com/eventos/Gerenciador/de/eventos/GerenciadorDeEventosApplication.java
@@ -0,0 +1,13 @@
+package com.eventos.Gerenciador.de.eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventos/Gerenciador/de/eventos/GerenciadorDeEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventos.Gerenciador.de.eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
